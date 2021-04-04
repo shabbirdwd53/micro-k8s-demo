11e2c5c200e52229e1e1e6d724476202e5e9d143
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2021-04-04 11:32:55 +0530

    Initial Commit

---
+++ service-registry/src/test/java/com/dailycodebuffer/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,9 @@
+package com.dailycodebuffer.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+}
